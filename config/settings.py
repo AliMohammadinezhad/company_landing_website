760483c64712c2e5a3db02f8aa6eff5ffe5d51a2
Ali Mohammadinezhad <alimn1382@hotmail.com>
2024-05-27 22:56:01 +0330

    fix inner-page.html and static settings

--- config/settings.py
+++ config/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'landing.apps.LandingConfig',
+    'landing',
 ]
 
 MIDDLEWARE = [
@@ -124,8 +124,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / '/static/'
+STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = [
     BASE_DIR / "statics",
 ]
