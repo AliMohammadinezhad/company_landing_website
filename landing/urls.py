ab2a421eeb2fa974dcae49036cc18566375f5163
Ali Mohammadinezhad <alimn1382@hotmail.com>
2024-05-28 18:43:33 +0330

    add view and url conf to landing app

--- landing/urls.py
+++ landing/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = 'landing'
+
 urlpatterns = [
-    path('', views.index_view, name='index')
+    path('', views.index_view, name='index'),
+    path('inner/', views.inner_view, name='inner')
+    
 ]
--- landing/views.py
+++ landing/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index_view(request):
     return render(request, 'landing/index.html')
+
+def inner_view(request):
+    return render(request, 'landing/inner-page.html')
